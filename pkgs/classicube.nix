ffff1c2f8abe3fe406228f448b6d95669cf38d0b
FormunaGit <formuna@tuta.io>
2026-02-21 22:11:32 -0400

    Small patch to classicube.nix

--- pkgs/classicube.nix
+++ pkgs/classicube.nix
@@ -3,10 +3,9 @@
   fetchurl,
   makeWrapper,
   steam-run,
-  lib,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "classicube";
   version = "1.3.8";
 
@@ -21,42 +20,41 @@ stdenv.mkDerivation rec {
   dontBuild = true;
 
   installPhase = ''
-    runHook preInstall
+        runHook preInstall
 
-    mkdir -p $out/bin
+        mkdir -p $out/bin
 
-    cat > $out/bin/classicube << 'WRAPPER_EOF'
-#!/usr/bin/env bash
-GAME_DIR="$HOME/.classicube"
-STORE_DIR="@out@/share/classicube"
+        cat > $out/bin/classicube << 'WRAPPER_EOF'
+    #!/usr/bin/env bash
+    GAME_DIR="$HOME/.amprpackages/classicube"
+    STORE_DIR="@out@/share/classicube"
 
-# Copy game files if not present or if store version is newer
-if [ ! -f "$GAME_DIR/ClassiCube" ]; then
-    mkdir -p "$GAME_DIR"
-    cp -r "$STORE_DIR"/* "$GAME_DIR"/
-    chmod -R u+w "$GAME_DIR"
-fi
+    # Copy game files if not present or if store version is newer
+    if [ ! -f "$GAME_DIR/ClassiCube" ]; then
+        mkdir -p "$GAME_DIR"
+        cp -r "$STORE_DIR"/* "$GAME_DIR"/
+        chmod -R u+w "$GAME_DIR"
+    fi
 
-cd "$GAME_DIR"
-exec STEAM_RUN/bin/steam-run ./ClassiCube "$@"
-WRAPPER_EOF
+    cd "$GAME_DIR"
+    exec STEAM_RUN/bin/steam-run ./ClassiCube "$@"
+    WRAPPER_EOF
 
-    substituteInPlace $out/bin/classicube \
-      --subst-var out \
-      --replace STEAM_RUN ${steam-run}
+        substituteInPlace $out/bin/classicube \
+          --subst-var out \
+          --replace STEAM_RUN ${steam-run}
 
-    mkdir -p $out/share/classicube
-    cp -r * $out/share/classicube/
+        mkdir -p $out/share/classicube
+        cp -r * $out/share/classicube/
 
-    chmod +x $out/bin/classicube
+        chmod +x $out/bin/classicube
 
-    runHook postInstall
+        runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     description = "A custom Minecraft Classic compatible client (binary wrapper)";
     homepage = "https://www.classicube.net/";
-    license = licenses.bsd3;
     platforms = [ "x86_64-linux" ];
     mainProgram = "classicube";
   };
